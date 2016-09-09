ccae84b9c897bde38fc196b001fbcada3421caf8
Rgerritse <ruben.gerritse@student.uva.nl>
2016-09-09 12:38:18 +0200

    Text, images and button for arms

--- app/src/main/java/com/example/r_ger/rubengerritse_pset1/MainActivity.java
+++ app/src/main/java/com/example/r_ger/rubengerritse_pset1/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.r_ger.rubengerritse_pset1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CheckBox armsCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        armsCheckBox = (CheckBox) findViewById(R.id.arms_checkbox);
+    }
+
+    public void onClickArmsCheckbox(View view) {
+
     }
 }
