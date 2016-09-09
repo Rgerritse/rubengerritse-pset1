cfe022e2c0bd054fe12de2c00bf7017f0ee59f67
Rgerritse <ruben.gerritse@student.uva.nl>
2016-09-09 13:35:54 +0200

    All buttons and images, layout equal

--- app/src/main/java/com/example/r_ger/rubengerritse_pset1/MainActivity.java
+++ app/src/main/java/com/example/r_ger/rubengerritse_pset1/MainActivity.java
@@ -10,15 +10,44 @@ import android.widget.RadioButton;
 public class MainActivity extends AppCompatActivity {
 
     private CheckBox armsCheckBox;
-
+    private ImageView armsImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         armsCheckBox = (CheckBox) findViewById(R.id.arms_checkbox);
+        armsImage = (ImageView) findViewById(R.id.arms_image);
     }
 
     public void onClickArmsCheckbox(View view) {
+        if (armsImage.getVisibility() == View.VISIBLE) {
+            armsImage.setVisibility(View.INVISIBLE);
+        } else {
+            armsImage.setVisibility(View.VISIBLE);
+        }
+    }
 
+    public void onClickEarsCheckbox(View view) {
+    }
+
+    public void onClickEyebrowsCheckbox(View view) {
+    }
+
+    public void onClickEyesCheckbox(View view) {
+    }
+
+    public void onClickGlassesCheckbox(View view) {
+    }
+
+    public void onClickHatCheckbox(View view) {
+    }
+
+    public void onClickMouthCheckbox(View view) {
+    }
+
+    public void onClickMustacheCheckbox(View view) {
+    }
+
+    public void onClickNoseCheckbox(View view) {
     }
 }
